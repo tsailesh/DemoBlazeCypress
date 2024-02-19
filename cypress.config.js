170ba85aac00324d3c5bf64dd4a899239b1e4ac9
tsailesh <ttsailesh@gmail.com>
2024-02-19 15:56:08 +0545

    Config added : baseUrl,pageLoadTimeout and defaultCommandTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.demoblaze.com/",
+    pageLoadTimeout: 100000,
+    defaultCommandTimeout:40000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
