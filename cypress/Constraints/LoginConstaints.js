176753c91a868bd6f709acc7734f7aa4bbda6b82
tsailesh <ttsailesh@gmail.com>
2024-02-24 18:51:59 +0545

    test cases added

--- cypress/Constraints/LoginConstaints.js
+++ cypress/Constraints/LoginConstaints.js
@@ -1,2 +1,4 @@
-export const USERNAME = 'hello1x2';
-export const PASSWORD = "hello";
+export const VALID_USERNAME = "hello1x2";
+export const VALID_PASSWORD = "hello";
+export const INVALID_USERNAME = "hello1x2x3x4";
+export const INVALID_PASSWORD = "hello1x2x3x4X5";
